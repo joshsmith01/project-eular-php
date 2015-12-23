ded6e44f7eee5fdb0d0021a7373d5b8948c9e6f4
Joshua Smith <joshsmith01@me.com>
2015-12-22 23:56:27 -0800

    Complete q-005

---
+++ q-005/q-005.php
@@ -0,0 +1,29 @@
+<!--2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.-->
+<!--What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?-->
+
+<?php
+$a = 2520;
+
+
+
+
+//echo $i;
+
+
+function isDivisible($a) {
+	foreach ( range( 11, 20 ) as $number ) {
+		if (($a % $number == 0)) {
+			continue;
+		} else {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+while ( isDivisible($a) == false ) {
+	$a += 2520;
+
+}
+echo $a . '<br>';
