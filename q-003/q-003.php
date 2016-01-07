af1ae3b8d3ab555570d7a47caf2a618d8a682834
Joshua Smith <joshsmith01@me.com>
2016-01-07 10:22:35 -0800

    Add timing functions to see how long the process takes to execute.

--- q-003/q-003.php
+++ q-003/q-003.php
@@ -7,10 +7,15 @@
 // What is the largest prime factor of the number 600851475143 ?
 
 
-// This function will determine if the number is evenly divisible. If it is it will echo out the number.
-//
+// This function will determine if the number is evenly divisible. The function only returns true or false depending on whether or not the number supplied is a prime number or not.
+
+$rustart = getrusage();
+
+
+
 
 function isPrime($n) {
+	// Iterate to half of the supplied number, rounded up if necessary. I can't use the square root like I had originally planned.
 	for($i=3; $i <= ceil($n/2); $i++) {
 		if( $n%$i == 0 ) {
 			return false;
@@ -20,13 +25,30 @@ function isPrime($n) {
 }
 
 
-$a = 13195;
+$a = 600851475143;
+//$a = 13195;
 for($i=3; $i <= floor($a/2); $i = $i + 2) {
 	if($a % $i == 0 ) {
 		$y = $a / $i;
 		if(isPrime($y)) {
-			echo $y.PHP_EOL;
+			echo $y . '<br>';
 			break;
 		}
 	}
 }
+
+
+
+
+
+// Script end
+function rutime( $ru, $rus, $index ) {
+	return ( $ru["ru_$index.tv_sec"] * 1000 + intval( $ru["ru_$index.tv_usec"] / 1000 ) )
+	       - ( $rus["ru_$index.tv_sec"] * 1000 + intval( $rus["ru_$index.tv_usec"] / 1000 ) );
+}
+
+$ru = getrusage();
+echo "This process used " . rutime( $ru, $rustart, "utime" ) .
+     " ms for its computations\n";
+echo "It spent " . rutime( $ru, $rustart, "stime" ) .
+     " ms in system calls\n";
